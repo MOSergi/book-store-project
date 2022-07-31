25c08e35c841c1925e51150dc8a1c38f838f5833
MOSergi <sergioorozmendez@gmail.com>
2022-07-31 13:06:26 +0200

    add icons to menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,14 @@ import { NavLink } from "react-router-dom";
 //estilos
 import "../styles/Navbar.css"; 
 //iconos
-import { FaHome, FaBars } from "react-icons/fa";
+import { FaHome, FaBars, FaStar, FaUserPlus, FaUserCheck} from "react-icons/fa";
 import { useState } from "react";
 
 export default function Navbar (){
 
     const [showMenu, setShowMenu] = useState<boolean>(false);
+
+    console.log("soy navbar");
 
     return (
         <nav id="menu">
@@ -16,11 +18,11 @@ export default function Navbar (){
                 <div id={showMenu ? "menuDivisionsMb" : "menuDivisions"}>
                     <div className="submenusections">
                         <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/"><FaHome /> Home</NavLink></li>
-                        <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/Recomendados">Recomendados</NavLink></li>
+                        <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/Recomendados">Recomendados <FaStar /></NavLink></li>
                     </div>
                     <div className="submenusections">
-                        <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/Registro">Registro</NavLink></li>
-                        <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/Login">Inicio Sesión</NavLink></li>
+                        <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/Registro">Registro <FaUserPlus /></NavLink></li>
+                        <li><NavLink className={({isActive})=>isActive ? "linkActive" : "links"} to="/Login">Inicio Sesión <FaUserCheck /></NavLink></li>
                     </div>
                 </div>
                 <h2 className="BurgerMenu" onClick={()=> showMenu ? setShowMenu(false) : setShowMenu(true)}><FaBars /></h2>
