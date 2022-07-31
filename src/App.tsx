edbc88da322349f328a6bd3833db9781e4676e8b
MOSergi <sergioorozmendez@gmail.com>
2022-07-31 11:20:09 +0200

    acabada la configuracion del entorno de desarrollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function App() {
+
+        const [count, setCount] = useState<number>(0);
+
         return (
             <div>
                 <h2>Hola</h2>
+                {count}
             </div>
         );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,49 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-
+    entry : "./src/index.tsx",
+    output : {
+        path : path.resolve(__dirname, "./build"),
+        filename : "bundle.js"
+    },
+    module : {
+        rules : [
+            {
+                test : /\.tsx?$/,
+                exclude : /(node_modules|browser_components)/,
+                use : {
+                    loader : "babel-loader",
+                    options : {
+                        presets : [
+                            "@babel/preset-env",
+                            [
+                                "@babel/preset-react", {
+                                    runtime : "automatic"
+                                }
+                            ],
+                            "@babel/preset-typescript"
+                        ]
+                    }
+                }
+            },
+            {
+                test : /\.css$/i,
+                use: [MiniCssExtractPlugin, "css-loader"]
+            }
+        ]
+    },
+    devServer : {
+        static : {
+            directory : path.join(__dirname, "./src/public")
+        }
+    },
+    resolve : {
+        extensions : [".tsx", ".ts", ".js"]
+    },
+    plugins : [new HtmlWebpackPlugin({
+        template : "./src/public/index.html"
+    }), 
+    new MiniCssExtractPlugin()]
 }
